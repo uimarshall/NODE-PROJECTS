2e9981a271440f6c27224c6262373b179d1f41ee
uimarshall <uimarshall@gmail.com>
2019-01-03 12:13:58 +0100

    added files for nodeComponents that shows diff inner working of comps of node functionalities that make up node.Js

---
+++ todoApp/app.js
@@ -0,0 +1,7 @@
+// pties & mtds available in the Global Obj of Node
+// console.log(__dirname);
+// console.log(__filename);
+// Module is simply a file
+const bundle = require("../nodeComponents/bundle");
+console.log(bundle.counter(["hat", 5, "cup"]));
+console.log(bundle.adder(5, 10));
--- todoApp/timerInterval.js
+++ todoApp/timerInterval.js
@@ -1,8 +1,8 @@
 var time = 0;
-var timer = setInterval(()=>{
-  time += 2;
-  console.log(time + ' seconds have passed')
-  if (time > 12) {
-    clearInterval(timer);
-  }
+var timer = setInterval(() => {
+	time += 2;
+	console.log(time + " seconds have passed");
+	if (time > 12) {
+		clearInterval(timer);
+	}
 }, 3000);
