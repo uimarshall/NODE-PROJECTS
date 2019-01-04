7754d4e992b29224f0085c72210ca52d559ed5b1
uimarshall <uimarshall@gmail.com>
2019-01-04 10:43:01 +0100

    added more files for nodeComponents

---
+++ todoApp/nodeComponents/pipeToUser.js
@@ -0,0 +1,17 @@
+var http = require("http");
+var fs = require("fs");
+var server = http.createServer(function(req, res) {
+	// We can access the 'url' pty on the 'req' obj
+	console.log(`Request was made: ${req.url}`);
+	// Response headers
+	res.writeHead(200, { "Content-Type": "text/plain" });
+	// Response body
+	var myReadStream = fs.createReadStream(__dirname + "/readMe.txt", "utf8");
+
+	// myReadStream pipes to 'res'
+	myReadStream.pipe(res);
+});
+
+// SPECIFY A PORT NUMBER FOR NODE TO LISTEN TO, so that node can the req is meant for him
+server.listen(3000, "127.0.0.1");
+console.log("Now listening to port 3000");
