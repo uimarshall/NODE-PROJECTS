555a85fa94ef35a904fced300362a9802453b961
uimarshall <uimarshall@gmail.com>
2019-01-06 23:47:56 +0100

    added files for connection to database

--- realtodoapp/controllers/todoControllers.js
+++ realtodoapp/controllers/todoControllers.js
@@ -3,26 +3,53 @@ var mongoose = require("mongoose");
 
 // Connect to db
 mongoose.connect("mongodb://test:test123@ds149914.mlab.com:49914/todo");
-var data = [
-	{ item: "stand up" },
-	{ item: "trello update" },
-	{ item: "git commit" }
-];
+// Create schema
+var todoSchema = new mongoose.Schema({
+	item: String
+});
+var Todo = mongoose.model("Todo", todoSchema);
+// var itemOne = Todo({ item: "but drinks" }).save(function(err) {
+// 	if (err) {
+// 		throw err;
+// 		console.log("item saved");
+// 	}
+// 	console.log("item saved");
+// });
+// var data = [
+// 	{ item: "stand up" },
+// 	{ item: "trello update" },
+// 	{ item: "git commit" }
+// ];
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 module.exports = app => {
 	// The function exported returns the routing, taking advantage of the app parameter.
 	// the function route to the 'views'(/todo)
 	app.get("/todo", (req, res) => {
-		res.render("todo", { todos: data });
+		// get data from mongodb and pass it to the view
+		// empty braces means find all entries in our coll in the db
+		Todo.find({}, function(err, data) {
+			if (err) throw err;
+
+			res.render("todo", { todos: data });
+		});
 	});
 	app.post("/todo", urlencodedParser, (req, res) => {
-		data.push(req.body);
-		res.json(data);
+		// get data from the view/form and add it to db
+		var newTodo = Todo(req.body).save(function(err, data) {
+			if (err) throw err;
+			res.json(data);
+		});
+		// data.push(req.body);
+		// res.json(data);
 	});
 	app.delete("/todo/:item", (req, res) => {
-		data = data.filter(function(todo) {
-			return todo.item.replace(/ /g, "-") !== req.params.item;
+		// delete the requestd item from db
+		Todo.find({ item: req.params.item.replace(/\-/g, " ") }).remove(function(
+			err,
+			data
+		) {
+			if (err) throw err;
+			res.json(data);
 		});
-		res.json(data);
 	});
 };
